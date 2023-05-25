c068fba24e411c6cc41fc8a6b5e7f05c0bb98415
NellyNel520 <channelharris520@gmail.com>
2023-05-25 01:24:24 -0400

    added image carosle with place holder images

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import LogoHeader from '../components/LogoHeader'
 import Promo from '../components/Promo'
+import  Slider  from '../components/Slider'
 
 const Home = () => {
   return (
     <div className=''>
     <LogoHeader />
     <Promo />
+    <Slider />
     </div>
     
   )
