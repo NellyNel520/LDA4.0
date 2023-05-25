85cd9d01c2d36c828ec3aef7d1d7f44eafd41e84
NellyNel520 <channelharris520@gmail.com>
2023-05-24 23:32:52 -0400

    added logo video

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = ({user,  handleLogOut}) => {
 	let userOptions
 	if (user) {
 	  userOptions = (
-	    <div className="shadow-md w-full fixed top-0 left-0">
+	    <div className="shadow-md w-full sticky top-0 left-0">
 			<div className="md:flex items-center justify-between bg-black text-blue-400 py-4 md:px-10 px-7">
 				<div
 					className="font-bold text-2xl cursor-pointer flex items-center font-ari
