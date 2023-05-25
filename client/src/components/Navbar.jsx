22b5a1fa2713812d08ed393e3d58623b4bbe0b82
NellyNel520 <channelharris520@gmail.com>
2023-05-24 20:27:23 -0400

    created request methods file

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import MenuIcon from '@mui/icons-material/Menu'
 import CloseIcon from '@mui/icons-material/Close'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
 import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined'
 import PersonIcon from '@mui/icons-material/Person'
 import { Link } from 'react-router-dom'
@@ -9,22 +9,22 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
 	let [open, setOpen] = useState(false)
 
-  let Links =[
-    {name:"HOME",link:"/"},
-    {name:"SERVICE",link:"/"},
-    {name:"ABOUT",link:"/about"},
-    {name:"BLOG'S",link:"/"},
-    {name:"CONTACT",link:"/"},
-  ];
+	let Links = [
+		{ name: 'HOME', link: '/' },
+		{ name: 'SERVICE', link: '/' },
+		{ name: 'ABOUT', link: '/about' },
+		{ name: "BLOG'S", link: '/' },
+		{ name: 'CONTACT', link: '/' },
+	]
 
-  // let userOptions 
-  // if (user) {
-  //   userOptions = (
-  //     <div className="shadow-md w-full  top-0 left-0">
+	// let userOptions
+	// if (user) {
+	//   userOptions = (
+	//     <div className="shadow-md w-full  top-0 left-0">
 	// 		<div className="md:flex items-center justify-between bg-black text-blue-400 py-4 md:px-10 px-7">
 	// 			<div
-	// 				className="font-bold text-2xl cursor-pointer flex items-center font-ari 
-  //     "
+	// 				className="font-bold text-2xl cursor-pointer flex items-center font-ari
+	//     "
 	// 			>
 	// 				London Dior Apparel
 	// 			</div>
@@ -33,7 +33,7 @@ const Navbar = () => {
 	// 				onClick={() => setOpen(!open)}
 	// 				className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
 	// 			>
-					
+
 	// 				{open ? <CloseIcon /> : <MenuIcon />}
 	// 			</div>
 
@@ -43,49 +43,48 @@ const Navbar = () => {
 	// 				}`}
 	// 			>
 	// 				{/* {
-  //         Links.map((link)=>(
-  //          <Link className='md:ml-8 text-xl md:my-0 my-7' to={link.link}>
+	//         Links.map((link)=>(
+	//          <Link className='md:ml-8 text-xl md:my-0 my-7' to={link.link}>
 	// 					<li className='text-blue-400 hover:text-gray-400 duration-500'>{link.name}</li>
 	// 				</Link>
-  //         ))
-  //       } */}
+	//         ))
+	//       } */}
 
-     
 	// 				<Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
 	// 					<li className='text-blue-400 hover:text-gray-400 duration-500' >
-  //             <ExitToAppOutlinedIcon />
-  //           </li>
+	//             <ExitToAppOutlinedIcon />
+	//           </li>
 	// 				</Link>
-  //         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
+	//         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
 	// 					<li className='text-blue-400 hover:text-gray-400 duration-500' >
-  //             <PersonIcon />
-  //           </li>
+	//             <PersonIcon />
+	//           </li>
 	// 				</Link>
-  //         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
+	//         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
 	// 					<li className='text-blue-400 hover:text-gray-400 duration-500' >HOME</li>
 	// 				</Link>
-  //         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
+	//         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
 	// 					<li className='text-blue-400 hover:text-gray-400 duration-500' >PRODUCTS</li>
 	// 				</Link>
-  //         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
+	//         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
 	// 					<li className='text-blue-400 hover:text-gray-400 duration-500' >ABOUT</li>
 	// 				</Link>
-  //         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
-  //           <li  className='text-blue-400 hover:text-gray-400 duration-500'><ShoppingCartIcon /></li>
-  //         </Link>
+	//         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
+	//           <li  className='text-blue-400 hover:text-gray-400 duration-500'><ShoppingCartIcon /></li>
+	//         </Link>
 	// 			</ul>
 	// 		</div>
 	// 	</div>
 
-  //   )
-  // }
+	//   )
+	// }
 
-  // const publicOptions = (
-  //   <div className="shadow-md w-full  top-0 left-0">
+	// const publicOptions = (
+	//   <div className="shadow-md w-full  top-0 left-0">
 	// 		<div className="md:flex items-center justify-between bg-black text-blue-400 py-4 md:px-10 px-7">
 	// 			<div
-	// 				className="font-bold text-2xl cursor-pointer flex items-center font-ari 
-  //     "
+	// 				className="font-bold text-2xl cursor-pointer flex items-center font-ari
+	//     "
 	// 			>
 	// 				London Dior Apparel
 	// 			</div>
@@ -94,7 +93,7 @@ const Navbar = () => {
 	// 				onClick={() => setOpen(!open)}
 	// 				className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
 	// 			>
-					
+
 	// 				{open ? <CloseIcon /> : <MenuIcon />}
 	// 			</div>
 
@@ -104,63 +103,60 @@ const Navbar = () => {
 	// 				}`}
 	// 			>
 	// 				{/* {
-  //         Links.map((link)=>(
-  //          <Link className='md:ml-8 text-xl md:my-0 my-7' to={link.link}>
+	//         Links.map((link)=>(
+	//          <Link className='md:ml-8 text-xl md:my-0 my-7' to={link.link}>
 	// 					<li className='text-blue-400 hover:text-gray-400 duration-500'>{link.name}</li>
 	// 				</Link>
-  //         ))
-  //       } */}
+	//         ))
+	//       } */}
 
-     
 	// 				<Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
 	// 					<li className='text-blue-400 hover:text-gray-400 duration-500' >REGISTER</li>
 	// 				</Link>
-  //         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
+	//         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
 	// 					<li className='text-blue-400 hover:text-gray-400 duration-500' >LOGIN</li>
 	// 				</Link>
-  //         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
+	//         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
 	// 					<li className='text-blue-400 hover:text-gray-400 duration-500' >HOME</li>
 	// 				</Link>
-  //         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
+	//         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
 	// 					<li className='text-blue-400 hover:text-gray-400 duration-500' >PRODUCTS</li>
 	// 				</Link>
-  //         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
+	//         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
 	// 					<li className='text-blue-400 hover:text-gray-400 duration-500' >ABOUT</li>
 	// 				</Link>
-  //         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
-  //           <li  className='text-blue-400 hover:text-gray-400 duration-500'><ShoppingCartIcon /></li>
-  //         </Link>
+	//         <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
+	//           <li  className='text-blue-400 hover:text-gray-400 duration-500'><ShoppingCartIcon /></li>
+	//         </Link>
 	// 			</ul>
 	// 		</div>
 	// 	</div>
-  // )
+	// )
 
 	return (
-    <div>
-
-<div className="shadow-md w-full  top-0 left-0">
-			<div className="md:flex items-center justify-between bg-black text-blue-400 py-4 md:px-10 px-7">
-				<div
-					className="font-bold text-2xl cursor-pointer flex items-center font-ari 
+		<div>
+			<div className="shadow-md w-full  top-0 left-0">
+				<div className="md:flex items-center justify-between bg-black text-blue-400 py-4 md:px-10 px-7">
+					<div
+						className="font-bold text-2xl cursor-pointer flex items-center font-ari 
       "
-				>
-					London Dior Apparel
-				</div>
+					>
+						London Dior Apparel
+					</div>
 
-				<div
-					onClick={() => setOpen(!open)}
-					className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
-				>
-					
-					{open ? <CloseIcon /> : <MenuIcon />}
-				</div>
+					<div
+						onClick={() => setOpen(!open)}
+						className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
+					>
+						{open ? <CloseIcon /> : <MenuIcon />}
+					</div>
 
-				<ul
-					className={`font-play md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-black md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-						open ? 'top-20 ' : 'top-[-490px]'
-					}`}
-				>
-					{/* {
+					<ul
+						className={`font-play md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-black md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+							open ? 'top-20 ' : 'top-[-490px]'
+						}`}
+					>
+						{/* {
           Links.map((link)=>(
            <Link className='md:ml-8 text-xl md:my-0 my-7' to={link.link}>
 						<li className='text-blue-400 hover:text-gray-400 duration-500'>{link.name}</li>
@@ -168,34 +164,40 @@ const Navbar = () => {
           ))
         } */}
 
-     
-					<Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
-						<li className='text-blue-400 hover:text-gray-400 duration-500' >
-              <ExitToAppOutlinedIcon />
-            </li>
-					</Link>
-          <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
-						<li className='text-blue-400 hover:text-gray-400 duration-500' >
-              <PersonIcon />
-            </li>
-					</Link>
-          <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
-						<li className='text-blue-400 hover:text-gray-400 duration-500' >HOME</li>
-					</Link>
-          <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
-						<li className='text-blue-400 hover:text-gray-400 duration-500' >PRODUCTS</li>
-					</Link>
-          <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
-						<li className='text-blue-400 hover:text-gray-400 duration-500' >ABOUT</li>
-					</Link>
-          <Link className='md:ml-8 text-xl md:my-0 my-7' to={'/'}>
-            <li  className='text-blue-400 hover:text-gray-400 duration-500'><ShoppingCartIcon /></li>
-          </Link>
-				</ul>
+						<Link className="md:ml-8 text-xl md:my-0 my-7" to={'/'}>
+							<li className="text-blue-400 hover:text-gray-400 duration-500">
+								<ExitToAppOutlinedIcon />
+							</li>
+						</Link>
+						<Link className="md:ml-8 text-xl md:my-0 my-7" to={'/'}>
+							<li className="text-blue-400 hover:text-gray-400 duration-500">
+								<PersonIcon />
+							</li>
+						</Link>
+						<Link className="md:ml-8 text-xl md:my-0 my-7" to={'/'}>
+							<li className="text-blue-400 hover:text-gray-400 duration-500">
+								HOME
+							</li>
+						</Link>
+						<Link className="md:ml-8 text-xl md:my-0 my-7" to={'/'}>
+							<li className="text-blue-400 hover:text-gray-400 duration-500">
+								PRODUCTS
+							</li>
+						</Link>
+						<Link className="md:ml-8 text-xl md:my-0 my-7" to={'/'}>
+							<li className="text-blue-400 hover:text-gray-400 duration-500">
+								ABOUT
+							</li>
+						</Link>
+						<Link className="md:ml-8 text-xl md:my-0 my-7" to={'/'}>
+							<li className="text-blue-400 hover:text-gray-400 duration-500">
+								<ShoppingCartIcon />
+							</li>
+						</Link>
+					</ul>
+				</div>
 			</div>
 		</div>
-
-    </div>
 	)
 }
 
---
+++ client/src/services/requestMethods.js
@@ -0,0 +1,20 @@
+import Axios from 'axios'
+
+const BASE_URL = 'http://localhost:3001/api' 
+
+const user = JSON.parse(localStorage.getItem('persist:root'))?.user
+const currentUser = user && JSON.parse(user).currentUser
+// const currentUser = JS/ON.parse(user).currentUser
+const TOKEN = currentUser?.accessToken
+
+// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+
+
+export const publicRequest = Axios.create({
+	baseURL: BASE_URL,
+})
+
+export const userRequest = Axios.create({
+	baseURL: BASE_URL,
+	headers: { token: `Bearer ${TOKEN}` },
+})
