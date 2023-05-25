06284be45eb9776fe2d86eee82914a6747081439
NellyNel520 <channelharris520@gmail.com>
2023-05-25 19:20:35 -0400

    created rating component with fontawsome icons

---
+++ client/src/components/Rating.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+const Rating = (props) => {
+  const { rating, numReviews } = props
+
+  return (
+    <div className="rating mt-4 text-yellow-400">
+      <span>
+        <i
+          className={
+            rating >= 1
+              ? 'fas fa-star'
+              : rating >= 0.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 2
+              ? 'fas fa-star'
+              : rating >= 1.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 3
+              ? 'fas fa-star'
+              : rating >= 2.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 4
+              ? 'fas fa-star'
+              : rating >= 3.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 5
+              ? 'fas fa-star'
+              : rating >= 4.5
+              ? 'fas fa-star-half-alt'
+              : 'far fa-star'
+          }
+        />
+      </span>
+      <span className='numReviews text-white text-xl'> {numReviews} reviews</span>
+    </div>
+  );
+}
+
+export default Rating
