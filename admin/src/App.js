2f17ef87bfa833ff06ee13c066a150f371d6a3ac
NellyNel520 <channelharris520@gmail.com>
2023-05-26 04:55:48 -0400

    created login page for admin panel

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,18 @@
 
 import './styles/index.css';
+import { Route, Routes } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { logout } from './redux/userRedux';
+import Login from './pages/Login';
+
 
 function App() {
   return (
     <div className="App">
-      App
+      <Routes>
+      <Route path="/" element={<Login />} />
+      </Routes>
     </div>
   );
 }
