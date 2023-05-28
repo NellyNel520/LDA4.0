fe8b925aaecde0a4d960e79c9d59bb263c8d7836
NellyNel520 <channelharris520@gmail.com>
2023-05-28 19:09:03 -0400

    installed recharts

--- admin/src/components/UserActivity.jsx
+++ admin/src/components/UserActivity.jsx
@@ -15,7 +15,7 @@ const UserActivity = () => {
             <div class="rounded bg-gray-200 dark:bg-gray-800 p-3">
             {/* Title */}
             <div class="flex justify-between py-1 text-black dark:text-white">
-                <h3 class="text-sm font-semibold">Recent Users</h3>
+                <h3 class="text-2xl font-abril font-semibold text-blue-500">Newest Users</h3>
                 <svg class="h-4 fill-current text-gray-600 dark:text-gray-500 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 10a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4z" /></svg>
               </div>
               <NewUsers />
