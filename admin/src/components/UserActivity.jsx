e686e1e6ac8e007c8f3cbaff7f6f35d22ef2fb09
NellyNel520 <channelharris520@gmail.com>
2023-05-28 20:25:10 -0400

    completed user actvity chart

--- admin/src/components/UserActivity.jsx
+++ admin/src/components/UserActivity.jsx
@@ -1,47 +1,103 @@
 import React from 'react'
 import NewUsers from './NewUsers'
+import UserChart from './UserChart'
+import {
+	LineChart,
+	Line,
+	XAxis,
+	CartesianGrid,
+	Tooltip,
+	ResponsiveContainer,
+} from 'recharts'
+import { useEffect, useMemo, useState } from 'react'
+import { userRequest } from '../services/requestMethods'
 
 const UserActivity = () => {
-  return (
-   <div>
-    <div class="grid grid-cols-1 md:grid-cols-1 xl:grid-cols-2 p-4 gap-4 text-black dark:text-white">
-          <div class="md:col-span-2 xl:col-span-3">
-            <h3 class="text-lg font-semibold">Task summaries of recent sprints</h3>
-          </div>
+	const [userStats, setUserStats] = useState([])
 
+	const MONTHS = useMemo(
+		() => [
+			'Jan',
+			'Feb',
+			'Mar',
+			'Apr',
+			'May',
+			'Jun',
+			'Jul',
+			'Agu',
+			'Sep',
+			'Oct',
+			'Nov',
+			'Dec',
+		],
+		[]
+	)
 
-          {/* Card 1 */}
-          <div class="md:col-span-2 xl:col-span-1">
-            <div class="rounded bg-gray-200 dark:bg-gray-800 p-3">
-            {/* Title */}
-            <div class="flex justify-between py-1 text-black dark:text-white">
-                <h3 class="text-2xl font-abril font-semibold text-blue-500">Newest Users</h3>
-                <svg class="h-4 fill-current text-gray-600 dark:text-gray-500 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 10a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4z" /></svg>
-              </div>
-              <NewUsers />
-           
+	useEffect(() => {
+		const getStats = async () => {
+			try {
+				const res = await userRequest.get('/users/stats')
+				res.data.map((item) =>
+					setUserStats((prev) => [
+						...prev,
+						{ name: MONTHS[item._id - 1], 'Active User': item.total },
+					])
+				)
+			} catch {}
+		}
+		getStats()
+		console.log(userStats)
+	}, [MONTHS])
 
+	return (
+		<div>
+			<div class="grid grid-cols-1 md:grid-cols-1 xl:grid-cols-2 p-4 gap-4 text-black dark:text-white">
+				<div class="md:col-span-2 xl:col-span-3">
+					<h3 class="text-lg font-semibold">
+						Task summaries of recent sprints
+					</h3>
+				</div>
 
-            </div>
-          </div>
+				{/* Card 1 */}
+				<div class="md:col-span-2 xl:col-span-1">
+					<div class="rounded bg-gray-200 dark:bg-gray-800 p-3">
+						{/* Title */}
+						<div class="flex justify-between py-1 text-black dark:text-white">
+							<h3 class="text-2xl font-abril font-semibold text-blue-500">
+								Newest Users
+							</h3>
+							<svg
+								class="h-4 fill-current text-gray-600 dark:text-gray-500 cursor-pointer"
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+							>
+								<path d="M5 10a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4z" />
+							</svg>
+						</div>
+						<NewUsers />
+					</div>
+				</div>
 
-          {/* Card 2 */}
-          <div>
-            <div class="rounded bg-gray-200 dark:bg-gray-800 p-3">
-            {/* title */}
-            <div class="flex justify-between py-1 text-black dark:text-white">
+				{/* Card 2 */}
+				<div>
+					<div class="rounded bg-gray-200 dark:bg-gray-800 p-3">
+						{/* title */}
+						{/* <div class="flex justify-between py-1 text-black dark:text-white">
                 <h3 class="text-sm font-semibold">Tasks in QA</h3>
                 <svg class="h-4 fill-current text-gray-600 dark:text-gray-500 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5 10a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4zm7 0a1.999 1.999 0 1 0 0 4 1.999 1.999 0 1 0 0-4z" /></svg>
-              </div>
-             
-            </div>
-          </div>
-          <div>
-       
-          </div>
-        </div>
-   </div>
-  )
+              </div> */}
+						<UserChart
+							data={userStats}
+							title="User Analytics"
+							grid
+							dataKey="Active User"
+						/>
+					</div>
+				</div>
+				<div></div>
+			</div>
+		</div>
+	)
 }
 
 export default UserActivity
---
+++ admin/src/components/UserChart.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+  LineChart,
+  Line,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+
+const UserChart = ({grid, data, title, dataKey}) => {
+  
+  return (
+    <div className='pr-5'>
+         
+      <h3 className="chartTitle font-play  text-2xl">{title}</h3>
+      <ResponsiveContainer width="100%" aspect={4/ 3} className="p-5">
+        <LineChart data={data} >
+          <XAxis dataKey="name" stroke="#1182F4" />
+          <Line type="monotone" dataKey={dataKey} stroke="#1182F4" />
+          <Tooltip />
+          {grid && <CartesianGrid stroke="#5A5A5A" strokeDasharray="5 5" />}
+        </LineChart>
+      </ResponsiveContainer>
+    
+    </div>
+  )
+}
+
+export default UserChart
