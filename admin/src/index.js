de7d741cefc219e78be171062d91c4190f09a51e
NellyNel520 <channelharris520@gmail.com>
2023-06-05 18:41:29 -0400

    added disable react dev tools to admin for deployment

--- admin/src/index.js
+++ admin/src/index.js
@@ -6,8 +6,9 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store, persistor } from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
